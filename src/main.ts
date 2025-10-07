41ab97129cb75506b33f885837a5ddbdb267a2d1
Jmike-dev <gmikekamau@gmail.com>
2025-10-07 19:18:07 +0300

    new portfolio website with react

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import {appConfig} from './routes/app.config';
-import {AppComponent} from './routes/app.component';
-
-import {bootstrapApplication} from '@angular/platform-browser';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-);
--- src/service/technologies/index.modal.ts
+++
@@ -1,4 +0,0 @@
-export interface TechnologyModal {
-    icon: string;
-    name: string;
-}
